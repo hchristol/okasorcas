0a6ff381a7429cc1a7314fc1f7bc7cd2acdd7baa
Herve <herve.christol@laposte.net>
2020-04-24 11:39:35 +0200

    default host for redis server

--- routes/redis.js
+++ routes/redis.js
@@ -2,7 +2,7 @@
 // redis init (persistent data game)
 var redis = require("redis");
 //exports.client = redis.createClient({'parser': 'javascript', 'return_buffers': true, 'detect_buffers': false});
-exports.client = redis.createClient();
+exports.client = redis.createClient(6379,"redisokasorcas");
 exports.client.on("error", function (err) {
     console.log("Error " + err);
 });
